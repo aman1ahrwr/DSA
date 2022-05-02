ec82e56a791f22ac49a1260925728ea6f5948600
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-05-02 02:19:15 -0700

    Create Moore Voting algorithm(Majority element).cpp

---
+++ Moore Voting algorithm(Majority element).cpp
@@ -0,0 +1,16 @@
+int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int element = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(count==0){
+                element = nums[i];
+            }
+            
+            if(nums[i]==element){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return element;
+    }
