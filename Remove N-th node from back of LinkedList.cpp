d284f43db31a94c9668ffc09ae6b35d199223247
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-05-08 00:43:37 -0700

    Create Remove N-th node from back of LinkedList.cpp

---
+++ Remove N-th node from back of LinkedList.cpp
@@ -0,0 +1,17 @@
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* temp = new ListNode();
+        temp->next = head;
+        ListNode* fast = temp;
+        ListNode* slow = temp;
+        for(int i=1; i<=n; i++){
+            fast=fast->next;
+        }
+        
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        
+        slow->next=slow->next->next;
+        return temp->next;
+    }
