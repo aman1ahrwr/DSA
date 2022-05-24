6bd2d2b39c0dbacab5204b32e8302e86cd162130
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-05-24 01:23:22 -0700

    Create All Permutations of a Array.cpp

---
+++ All Permutations of a Array.cpp
@@ -0,0 +1,18 @@
+void recur(int ind, vector<int> &nums, vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=ind; i<nums.size(); i++){
+            swap(nums[ind], nums[i]);
+            recur(ind+1, nums, ans);
+            swap(nums[i], nums[ind]);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        recur(0, nums, ans);
+        return ans;
+    }
