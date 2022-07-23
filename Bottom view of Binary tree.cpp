0c0af6867ab064360e3573a9b598ef6c4ea26856
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-07-23 03:47:20 -0700

    Create Bottom view of Binary tree.cpp

---
+++ Bottom view of Binary tree.cpp
@@ -0,0 +1,27 @@
+ vector <int> bottomView(Node *root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        map<int, int> map;
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+        
+        while(!q.empty()){
+            auto t = q.front();
+            q.pop();
+            Node* node = t.first;
+            int line = t.second;
+            map[line] = node->data;
+            
+            if(node->left!=NULL){
+                q.push({node->left, line-1});
+            }
+            if(node->right!=NULL){
+                q.push({node->right, line+1});
+            }
+        }
+        
+        for(auto it:map){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
