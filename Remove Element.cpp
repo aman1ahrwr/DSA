09a842369d5f381e56e4ed05c85b0c12a633d88e
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-08-21 07:44:17 -0700

    Create Remove Element.cpp

---
+++ Remove Element.cpp
@@ -0,0 +1,11 @@
+int removeElement(vector<int>& nums, int val) {
+        int count=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==val){
+                count++;
+            }else{
+                nums[i-count]=nums[i];
+            }
+        }
+        return nums.size()-count;
+    }
