2788ff6f609f1ed363634ea9d5ccd0a0a20df10a
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-08-26 08:06:17 -0700

    Create Huffman Encoding.cpp

---
+++ Huffman Encoding.cpp
@@ -0,0 +1,60 @@
+struct Node{
+        public:
+        int data;
+        Node* left;
+        Node* right;
+        
+        Node(int d){
+            data=d;
+            left=NULL;
+            right=NULL;
+        }
+    };
+    
+class cmp{
+    public:
+    bool operator()(Node* a, Node* b){
+        return a->data>b->data;
+    }
+};
+    
+    
+    void traverse(Node* root, string temp, vector<string> &ans){
+        if(root->left==NULL && root->right==NULL){
+            ans.push_back(temp);
+            return;
+        }
+        
+        traverse(root->left, temp+'0', ans);
+        traverse(root->right, temp+'1', ans);
+    }
+    
+	public:
+		vector<string> huffmanCodes(string S,vector<int> f,int N)
+		{
+		    priority_queue<Node*, vector<Node*>, cmp> pq;
+		    
+		    for(int i =0; i<N; i++){
+		        Node* temp = new Node(f[i]);
+		        pq.push(temp);
+		    }
+		    
+		    while(pq.size()>1){
+		        Node* left = pq.top();
+		        pq.pop();
+		        
+		        Node* right=pq.top();
+		        pq.pop();
+		        
+		        Node* newNode = new Node(left->data+right->data);
+		        newNode->left=left;
+		        newNode->right=right;
+		        pq.push(newNode);
+		    }
+		    
+		    Node* root = pq.top();
+		    vector<string> ans;
+		    string temp="";
+		    traverse(root, temp, ans);
+		    return ans;
+		}
