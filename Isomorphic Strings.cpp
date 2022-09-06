beae73731618af25d3da0dbe3ca8fcca1ee99078
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-09-06 11:29:45 -0700

    Create Isomorphic Strings.cpp

---
+++ Isomorphic Strings.cpp
@@ -0,0 +1,15 @@
+bool areIsomorphic(string str1, string str2)
+    {
+        if(str1.size()!=str2.size()) return false;
+        unordered_map<char, char> map1;
+        unordered_map<char, char> map2;
+        for(int i=0; i<str1.size(); i++){
+            if(map1.find(str1[i])==map1.end() && map2.find(str2[i])==map2.end()){
+                map1[str1[i]] = str2[i];
+                map2[str2[i]] = str1[i];
+            }else if(map1[str1[i]]!=str2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
