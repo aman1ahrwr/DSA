1343d687df625898b0d8458184c2218aa9f69854
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-10-16 03:19:50 -0700

    Create Remove All Occurrences of a Substring.cpp

---
+++ Remove All Occurrences of a Substring.cpp
@@ -0,0 +1,6 @@
+string removeOccurrences(string s, string part) {
+        while(s.size()!=0 && s.find(part)<s.size()){
+            s.erase(s.find(part), part.size());
+        }
+        return s;
+    }
