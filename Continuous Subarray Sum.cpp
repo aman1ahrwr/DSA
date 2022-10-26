d2b72e9eb9dfb24dd257839e1100a5d540b5d017
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-10-26 08:55:49 -0700

    Update Continuous Subarray Sum.cpp

--- Continuous Subarray Sum.cpp
+++ Continuous Subarray Sum.cpp
@@ -1,9 +1,11 @@
 bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> map = {{0, -1}};
+        unordered_map<int, int> map;
         int sum=0;
         for(int i=0; i<nums.size(); i++){
             sum+=nums[i];
             sum%=k;
+            
+            if(sum==0 && i) return true;
             if(map.find(sum)!=map.end()){
                 if(i-map[sum]>1){
                     return true;
@@ -14,6 +16,4 @@ bool checkSubarraySum(vector<int>& nums, int k) {
             }
         }
         return false;
-        
-        
     }
