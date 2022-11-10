926afa0f9b3a59081bfbb94be70655cef9043130
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-11-10 02:09:15 -0800

    Create Union of two arrays.cpp

---
+++ Union of two arrays.cpp
@@ -0,0 +1,13 @@
+int doUnion(int a[], int n, int b[], int m)  {
+        unordered_set<int> s;
+        
+        for(int i=0; i<n; i++){
+        s.insert(a[i]);
+        }
+        
+        for(int j=0; j<m; j++){
+        s.insert(b[j]);
+        }
+        
+        return s.size();
+    }
