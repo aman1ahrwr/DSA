131ebc916a2099d2d58a31b2bd192950b57ebbd2
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-11-14 04:07:46 -0800

    Create Count triplets with sum smaller than X.cpp

---
+++ Count triplets with sum smaller than X.cpp
@@ -0,0 +1,19 @@
+long long countTriplets(long long arr[], int n, long long sum)
+	{
+	    long long count=0;
+	    sort(arr, arr+n);
+	    for(int i=0; i<n; i++){
+	        int j=i+1;
+	        int k=n-1;
+	        while(j<k){
+	            if(arr[i]+arr[j]+arr[k]<sum){
+	                count+=k-j;
+	                j++;
+	            }else {
+	                k--;
+	            }
+	        }
+	    }
+	    
+	    return count;
+	}
