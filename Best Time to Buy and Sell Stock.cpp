4aba2ef4ded60863a234543f7d4daecd8b314301
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2022-12-27 02:09:03 -0800

    Create Best Time to Buy and Sell Stock.cpp

---
+++ Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,11 @@
+int maxProfit(vector<int>& prices) {
+        int minSoFar=INT_MAX;
+        int ans=0;
+
+        for(int i=0; i<prices.size(); i++){
+            minSoFar=min(minSoFar, prices[i]);
+            ans=max(ans, prices[i]-minSoFar);
+        }
+
+        return ans;
+    }
