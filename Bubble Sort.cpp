80fb8543e94f405a171e97a756cf80a8d519ef7a
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-05 01:10:10 -0800

    Update and rename Bubble Sort to Bubble Sort.cpp

---
+++ Bubble Sort.cpp
@@ -0,0 +1,12 @@
+void bubbleSort(int arr[], int n)
+{
+    for(int i=1; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }   
+}
