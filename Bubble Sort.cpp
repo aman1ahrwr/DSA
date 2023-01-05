45adaba88473073731d95b1c39e5ab2ae848727b
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-05 01:19:08 -0800

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,12 +1,15 @@
 void bubbleSort(int arr[], int n)
 {
     for(int i=1; i<n; i++){
+        int swapped = 0;
         for(int j=0; j<n-i; j++){
             if(arr[j]>arr[j+1]){
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
+                swapped=1;
             }
         }
+        if(swapped==0) break;
     }   
 }
