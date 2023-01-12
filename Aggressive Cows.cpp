aab66ab630cf6dc7962756db50bcaafce6953262
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-12 02:25:10 -0800

    Update Aggressive Cows.cpp

--- Aggressive Cows.cpp
+++ Aggressive Cows.cpp
@@ -27,3 +27,40 @@ int largestMinCows(int a[], int n, int cows){
       }
     return high;
 }
+
+//Better Slightly
+bool isPossible(vector<int> &stalls, int k, int n, int mid){
+    int cows = 1;
+    int initialPos = stalls[0];
+    for(int i=1; i<n; i++){
+        if(stalls[i]-initialPos>=mid){
+            initialPos=stalls[i];
+            cows++;
+            if(cows==k){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(), stalls.end());
+    int low=0;
+    int n=stalls.size();
+    int high = stalls[n-1];
+    int ans=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        
+        if(isPossible(stalls, k, n, mid)){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    
+    return ans;
+}
