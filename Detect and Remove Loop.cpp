d1ab5b2d19ee3259286e3a95068381f2acb6185c
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-17 03:31:48 -0800

    Create Detect and Remove Loop.cpp

---
+++ Detect and Remove Loop.cpp
@@ -0,0 +1,26 @@
+Node *removeLoop(Node *head)
+{
+    Node* slow=head;
+    Node*fast =head;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            break;
+        }
+    }
+    
+    if(slow!=fast) return head;
+    
+    slow=head;
+    while(slow!=fast){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    
+    while(fast->next!=slow){
+        fast=fast->next;
+    }
+    fast->next=NULL;
+    return head;
+}
