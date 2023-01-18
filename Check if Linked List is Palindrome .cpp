5a69ca52b436af61fe25695051780668c18f74b1
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-18 06:26:21 -0800

    Update Check if Linked List is Palindrome .cpp

--- Check if Linked List is Palindrome .cpp
+++ Check if Linked List is Palindrome .cpp
@@ -1,4 +1,45 @@
- bool isPalindrome(Node *head)
+// Better Solution.
+Node* reverse(Node* head){
+        Node* prev=NULL;
+        Node* temp=NULL;
+        Node* curr=head;
+        
+        while(curr){
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        
+        return prev;
+    }
+    
+    bool isPalindrome(Node *head)
+    {
+        Node* slow=head;
+        Node* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        fast=head;
+        slow = reverse(slow); 
+        
+        while(fast && slow){
+            if(fast->data!=slow->data){
+                return false;
+            }
+            fast=fast->next;
+            slow=slow->next;
+        }
+        
+        return true;
+    }
+
+
+// Same but less Readable Solution.
+bool isPalindrome(Node *head)
     {
         if(head==NULL || head->next==NULL){
             return 1;
