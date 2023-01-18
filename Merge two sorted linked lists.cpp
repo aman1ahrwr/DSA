f787dd3c01e14fdf1be7e208a2a33e065db25ada
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-18 04:32:37 -0800

    Update Merge two sorted linked lists.cpp

--- Merge two sorted linked lists.cpp
+++ Merge two sorted linked lists.cpp
@@ -23,3 +23,48 @@ Node* sortedMerge(Node* head1, Node* head2)
    
    
 }  
+
+
+// Another Approach
+Node<int>* solve(Node<int>* &head1, Node<int>* &head2){
+    if(head1->next==NULL){
+        head1->next=head2;
+        return head1;
+    }
+    
+    Node<int>* curr1=head1;
+    Node<int>* next1=head1->next;
+    Node<int>* curr2=head2;
+    Node<int>* next2=NULL;
+    
+    while(curr2 && next1){
+        if((curr2->data>=curr1->data) && (curr2->data<=next1->data)){
+            curr1->next=curr2;
+            next2=curr2->next;
+            curr2->next=next1;
+            
+            curr1=curr2;
+            curr2=next2;
+        }else{
+            curr1=next1;
+            next1=next1->next;
+            if(next1==NULL){
+                curr1->next=curr2;
+                return head1;
+            }
+        }
+    }
+    return head1;
+}
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    if(first==NULL) return second;
+    if(second==NULL) return first;
+    
+    if(first->data<=second->data){
+        return solve(first, second);
+    }else{
+        return solve(second, first);
+    }
+}
