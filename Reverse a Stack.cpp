9aa4e063ccdce356c15f5fecef8090e69c22148d
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-20 06:04:29 -0800

    Update Reverse a Stack.cpp

--- Reverse a Stack.cpp
+++ Reverse a Stack.cpp
@@ -14,3 +14,31 @@ void solve(stack<int> &st, stack<int> &St){
         solve(st, St);
         return st;
     }
+
+// Using Recursion
+void insertAtBottom(stack<int> &stack, int t){
+    if(stack.empty()){
+        stack.push(t);
+        return;
+    }
+    
+    int x=stack.top();
+    stack.pop();
+    
+    insertAtBottom(stack, t);
+    
+    stack.push(x);
+}
+
+void reverseStack(stack<int> &stack) {
+    if(stack.empty()){
+        return;
+    }
+    
+    int t = stack.top();
+    stack.pop();
+    
+    reverseStack(stack);
+    
+    insertAtBottom(stack, t);
+}
