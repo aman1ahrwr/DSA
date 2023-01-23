607c5cad0e91d26c095a17a60ffcecef1b81cf96
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-23 07:18:27 -0800

    Update The Celebrity Problem.cpp

--- The Celebrity Problem.cpp
+++ The Celebrity Problem.cpp
@@ -1,3 +1,46 @@
+// Easy to understand Approach usng stack
+int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            st.push(i);
+        }
+        
+        while(st.size()!=1){
+            int a = st.top();
+            st.pop();
+            
+            int b = st.top();
+            st.pop();
+            
+            if(M[a][b]==1){
+                st.push(b);
+            }else{
+                st.push(a);
+            }
+        }
+        
+        int ans = st.top();
+    
+        for(int i=0; i<n; i++){
+            if(M[i][ans]==0 && i!=ans){
+                return -1;
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            if(M[ans][i]!=0){
+                return -1;
+            }
+        }
+        
+        
+        return ans;
+    }
+
+
+
+// Without stack
 int celebrity(vector<vector<int> >& M, int n) 
     {
             int c=0;
