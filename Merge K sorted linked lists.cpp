0f5000c41292f2d79a67039f5decab083e3c60ff
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-30 00:59:57 -0800

    Update Merge K sorted linked lists.cpp

--- Merge K sorted linked lists.cpp
+++ Merge K sorted linked lists.cpp
@@ -1,3 +1,41 @@
+// Best Approach
+#include<queue>
+class compare{
+    public:
+    bool operator()(Node<int>* a, Node<int>* b){
+        return a->data>b->data;
+    }
+};
+
+Node<int>* mergeKLists(vector<Node<int>*> &listArray)
+{
+    Node<int>* ans= new Node<int>(-1);
+    Node<int>* temp=ans;
+    priority_queue<Node<int>*, vector<Node<int>*>, compare> pq;
+
+    for(int i=0; i<listArray.size(); i++){
+        if(listArray[i]!=NULL) 
+            pq.push(listArray[i]);
+    }
+    
+    while(pq.size()){
+        Node<int>* top = pq.top();
+        temp->next=top;
+        pq.pop();
+        temp=temp->next;
+
+        if(top->next){
+            pq.push(top->next);
+        }
+    }
+
+    return ans->next;
+}
+
+
+
+
+// Earlier Approach
 Node * mergeKLists(Node *arr[], int k)
     {
            Node* temp = new Node(0);
