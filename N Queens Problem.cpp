3ed286d6c506675a4bf1f1d44333a128adfbe5f2
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-01-31 01:47:14 -0800

    Update N Queens Problem.cpp

--- N Queens Problem.cpp
+++ N Queens Problem.cpp
@@ -1,3 +1,73 @@
+// Approach without space, Easy to understand.
+#include <bits/stdc++.h> 
+
+void copy(vector<vector<int>> &ans, vector<vector<int>> &board, int n){
+	vector<int> temp;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            temp.push_back(board[i][j]);
+        }
+    }
+    ans.push_back(temp);
+}
+
+bool isSafe(int row, int col, vector<vector<int>> &board, int n){
+	int x=row;
+	int y=col;
+
+	while(y>=0){
+		if(board[x][y]==1){
+			return false;
+		}
+		y--;
+	}
+
+ 	x=row;
+	y=col;
+	while(y>=0&& x>=0){
+		if(board[x][y]==1){
+			return false;
+		}
+		x--; y--;
+	}
+
+	x=row;
+	y=col;
+	while(y>=0 && x<n){
+		if(board[x][y]==1){
+			return false;
+		}
+		x++; y--;
+	}
+	return true;
+}
+
+void solve(vector<vector<int>> &ans, vector<vector<int>> &board, int col, int n){
+	if(col==n){
+		copy(ans, board, n);
+		return;
+	}
+
+	for(int row=0; row<n; row++){
+		if(isSafe(row, col, board, n)){
+			board[row][col]=1;
+			solve(ans, board, col+1, n);
+			board[row][col]=0;
+		}
+	}
+}
+
+vector<vector<int>> nQueens(int n)
+{
+	vector<vector<int>> ans;
+	vector<vector<int>> board(n, vector<int> (n, 0));
+	int col=0;
+	solve(ans, board, col, n);
+	return ans;
+}
+
+
+// Approach Using Space
 void solve(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &leftRow, vector<int> &upperDiagonal, vector<int> &lowerDiagonal, int n){
         if(col==n){
             ans.push_back(board);
