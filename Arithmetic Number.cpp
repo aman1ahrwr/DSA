0f6e76d313165e45906b9561eb5ccee86b9b3b66
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-02-05 01:29:55 -0800

    Create Arithmetic Number.cpp

---
+++ Arithmetic Number.cpp
@@ -0,0 +1,12 @@
+int inSequence(int A, int B, int C){
+        if(C==0){
+            return A==B;
+        }
+        
+        if((B-A)/C>=0 && (B-A)%C==0){
+            return 1;
+        }
+        
+        
+        return 0;
+    }
