8ddd5fcd228887bbd222fa71f607ac28a17e634d
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-02-13 17:07:42 +0530

    Update Fractional Knapsack.cpp

--- Fractional Knapsack.cpp
+++ Fractional Knapsack.cpp
@@ -1,3 +1,32 @@
+// Easy to Undeerstand Solution but Same Approach
+bool static compare(struct Item a, struct Item b){
+        double i=((double)a.value/(double)a.weight);
+        double j=((double)b.value/(double)b.weight);
+        
+        return i>j;
+    }
+    
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        
+        sort(arr, arr+n, compare);
+        
+        double ans=0.0;
+        for(int i=0; i<n; i++){
+            if(arr[i].weight<=W){
+                ans+=arr[i].value;
+                W-=arr[i].weight;
+            }else{
+                ans+=(double)W*(arr[i].value/(double)arr[i].weight);
+                break;
+            }
+        }
+        
+        return ans;
+    }
+
+// Earlier Solution
 bool static comparison(struct Item a, struct Item b){
         double a1 = (double)a.value/(double)a.weight;
         double b1 = (double)b.value/(double)b.weight;
