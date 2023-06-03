355ba58e41769b5c7d1f191752768ad99e6b2db8
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-03 17:20:40 +0530

    Create Pattern 14.cpp

---
+++ Pattern 14.cpp
@@ -0,0 +1,8 @@
+void printTriangle(int n) {
+        for(int i=0; i<n; i++){
+            for(char ch='A'; ch<='A'+i; ch++){
+                cout<<ch; 
+            }
+            cout<<endl;
+        }
+    }
