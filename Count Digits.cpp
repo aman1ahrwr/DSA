80e865a1921c22f0589792f6f611da2da2be89a4
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-12 16:57:56 +0530

    Create Count Digits.cpp

---
+++ Count Digits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        int t = N;
+        int cnt=0;
+        while(t!=0){
+            int d = t%10;
+            if(d>0 && N%d==0){
+                cnt++;
+            }
+            t/=10;
+        }
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
