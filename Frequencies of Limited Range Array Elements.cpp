eb1a60a42665c16aa1bcac70cfb4fec0f2c21943
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-15 18:41:00 +0530

    Create Frequencies of Limited Range Array Elements.cpp

---
+++ Frequencies of Limited Range Array Elements.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        unordered_map<int, int> mpp(P);
+        for(int i=0; i<N; i++){
+            mpp[arr[i]]++;
+        }
+        
+        for(int i=0; i<N; i++){
+            arr[i]=mpp[i+1];
+        }
+    }
+};
