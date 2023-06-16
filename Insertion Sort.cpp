5ea53babca612c31e3ffc85eeeb63069e74d1927
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-16 22:57:29 +0530

    Update and rename Insertion Sort to Insertion Sort.cpp
    Choose the appropriate place for the element and Insert it there.
    
    Steps ->
    
    - Run a Loop across the Array
    - Check the right place for it the the left side of array
    - Check it only 1st (2nd Element), no need to check for 0 index as there no element or index for swap with.

---
+++ Insertion Sort.cpp
@@ -0,0 +1,10 @@
+void insertionSort(int arr[], int n)
+    {
+        for(int i=0; i<n; i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                swap(arr[j-1], arr[j]);
+                j--;
+            }
+        }
+    }
