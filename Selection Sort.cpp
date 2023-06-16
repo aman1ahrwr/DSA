d4c8171dc064be21ddd0f405745a67f0a988fcd6
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-16 22:17:18 +0530

    Update and rename Selection Sort to Selection Sort.cpp

---
+++ Selection Sort.cpp
@@ -0,0 +1,12 @@
+void selectionSort(int arr[], int n)
+    {
+       for(int i=0; i<n-1; i++){
+           int miniInd = i;
+           for(int j=i; j<n; j++){
+               if(arr[miniInd]>arr[j]){
+                   miniInd=j;
+               }
+           }
+           swap(arr[i], arr[miniInd]);
+       }
+    }
