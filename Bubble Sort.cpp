e0a7120398ec11a40d215709db4cedce95a299a5
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-19 17:35:58 +0530

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,3 +1,4 @@
+// Iterative Approach to Sort using Bubble Sort
 void bubbleSort(int arr[], int n)
 {
     for(int i=1; i<n; i++){
@@ -16,3 +17,29 @@ void bubbleSort(int arr[], int n)
 
 // TC(B) = O(n), TC(W, Avg) = O(n^2)
 // SC=O(1)
+
+// Recursive Approach, complete outer loop with the help of Recursion
+void recursiveBubble(int arr[], int n){
+        if(n==1){
+            return;
+        }
+        
+        int swapBool = false;
+        for(int j=0; j<n-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapBool=true;
+            }
+        }
+        
+        if(swapBool==false){
+            return;
+        }
+        
+        recursiveBubble(arr, n-1);
+    }
+    
+    void bubbleSort(int arr[], int n)
+    {
+        recursiveBubble(arr, n); 
+    }
