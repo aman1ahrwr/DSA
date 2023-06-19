71ad87e74cdbf4acc1980d8239323809f4e20597
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-19 17:42:01 +0530

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,3 +1,4 @@
+// Iterative Approach to Sort using Insertion Sort
 void insertionSort(int arr[], int n)
     {
         for(int i=0; i<n; i++){
@@ -11,3 +12,27 @@ void insertionSort(int arr[], int n)
 
 // Worst and Best Case TC = O(n^2), Best Case TC = O(n)
 // SC=O(1)
+
+
+// Recursive Approach to Sort using Insertion Sort
+void insert(int arr[], int i, int n)
+    {
+        if(i==n){
+            return;
+        }
+        
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            swap(arr[j], arr[j-1]);
+            j--;
+        }
+        
+        insert(arr, i+1, n);
+    }
+
+
+    void insertionSort(int arr[], int n)
+    {
+        int i=0;
+        insert(arr, i, n);
+    }
