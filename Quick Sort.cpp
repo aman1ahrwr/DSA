d7c47aa0e8847af4cd0cfd295304a87a9960510d
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-19 18:41:02 +0530

    Update Quick Sort.cpp

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -1,18 +1,3 @@
-//{ Driver Code Starts
-#include <stdio.h>
-#include <bits/stdc++.h>
-using namespace std;
-
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-// } Driver Code Ends
 class Solution
 {
     public:
@@ -56,20 +41,6 @@ class Solution
     }
 };
 
+// TC = O(N*logN)
+// SC = O(1)
 
-//{ Driver Code Starts.
-int main()
-{
-    int arr[1000],n,T,i;
-    scanf("%d",&T);
-    while(T--){
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-      Solution ob;
-    ob.quickSort(arr, 0, n-1);
-    printArray(arr, n);
-    }
-    return 0;
-}
-// } Driver Code Ends
