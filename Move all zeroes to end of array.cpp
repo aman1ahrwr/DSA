6d35474a9b57406dbfab491dad072dcd9f937c5f
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-06-22 16:11:49 +0530

    Create Move all zeroes to end of array.cpp

---
+++ Move all zeroes to end of array.cpp
@@ -0,0 +1,21 @@
+void pushZerosToEnd(int arr[], int n) {
+	    int j=-1;
+	    for(int i=0; i<n; i++){
+	        if(arr[i]==0){
+	            j=i;
+	            break;
+	        }
+	    }
+	    
+	    if(j==-1) return;
+	    
+	    for(int i=j+1; i<n; i++){
+	        if(arr[i]!=0){
+	            swap(arr[j], arr[i]);
+	            j++;	
+	        }
+	    }
+	}
+	
+// 	TC=O(N)
+//  SC=O(1)
