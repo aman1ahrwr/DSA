6e1403af21001e83081babc632e31b50f3cb47bf
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-07-06 17:02:00 +0530

    Create Rotate by 90 degree.cpp

---
+++ Rotate by 90 degree.cpp
@@ -0,0 +1,19 @@
+
+class Solution
+{   
+    public:
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int> >& matrix, int n) 
+    { 
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        reverse(matrix.begin(), matrix.end());
+    } 
+
+// TC=O(n^2)
+// SC=O(1)
+};
