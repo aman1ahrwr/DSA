3a061dd89584a6bd1dd7c638b9b8d18fd4ed2113
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-07-11 15:13:15 +0530

    Update Pascal's Triangle.cpp

--- Pascal's Triangle.cpp
+++ Pascal's Triangle.cpp
@@ -11,3 +11,32 @@ vector<vector<int>> generate(int n) {
         }
         return ans;
     }
+
+
+// ALternative Solution using nCr principle
+class Solution {
+public:
+    vector<int> generateRows(int r){
+        vector<int> ans;
+        long long int res=1;
+        ans.push_back(res);
+        for(int i=1; i<r; i++){
+            res=res*(r-i);
+            res=res/i;
+            ans.push_back(res);
+        }
+        return ans;
+    }
+
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+        for(int i=1; i<=n; i++){
+            ans.push_back(generateRows(i));
+        }
+
+        return ans;
+    }
+};
+
+// TC=O(R*C)
+// SC=O(1)
