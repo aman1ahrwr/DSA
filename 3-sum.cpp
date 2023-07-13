7735e8b6c254e470c9901afe8040a495f6276650
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-07-13 16:46:20 +0530

    Update 3-sum.cpp

--- 3-sum.cpp
+++ 3-sum.cpp
@@ -1,65 +1,30 @@
-vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-        for(int i=0; i<nums.size()-2; i++){
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-                int low = i+1;
-                int high = nums.size()-1;
-                int sum = 0-nums[i];
-                while(low<high){
-                    if(nums[low]+nums[high]==sum){
-                        vector<int> temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[low]);
-                        temp.push_back(nums[high]);
-                        res.push_back(temp);
-                        
-                        while(low<high && nums[low]==nums[low+1]) low++;
-                        while(low<high && nums[high]==nums[high-1]) high--;
-                        
-                        low++,high--;
-                    }else if(nums[low]+nums[high]<sum){
-                        low++;
-                    }else{
-                        high--;
-                    }
-                }
-            }
-        }
-        return res;
-    }
-
-
-// Second Approach with Same logic
-vector<vector<int>> findTriplets(vector<int>arr, int n, int S) {
-	vector<vector<int>> ans;
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
     sort(arr.begin(), arr.end());
-    for(int i=0; i<n; i++){
-        int s=S-arr[i];
-        int j=i+1;
-        int k=n-1;
-        while(j<k){
-            if(arr[j]+arr[k]==s){
-                int x=arr[j];
-                int y=arr[k];
-                ans.push_back({arr[i], x, y});
-                j++;
-                k--;
-                while(j<k && arr[j]==x){
+    vector<vector<int>> ans;
+    for(int i=0; i<n-2; i++){
+        if(i==0 || (i>0 && arr[i]!=arr[i-1])){
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=arr[i]+arr[j]+arr[k];
+                if(sum==0){
+                    vector<int> temp={arr[i], arr[j], arr[k]};
+                    ans.push_back(temp);
+                    j++; k--;
+                    while(j<k && arr[j]==arr[j-1]) j++;
+                    while(j<k && arr[k]==arr[k+1]) k--;
+                }else if(sum<0){
                     j++;
-                }
-                while(j<k && arr[j]==y){
+                }else{
                     k--;
                 }
-            }else if(arr[j]+arr[k]<s){
-                j++;
-            }else{
-                k--;
             }
         }
-        while(i<n && arr[i]==arr[i+1]){
-            i++;
-        }
     }
+
     return ans;
 }
+
+// TC=O(N^2)
+// SC=O(K) where K is the number of triplets
