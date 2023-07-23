0d965fef46f19e32b3e0c2ca2fd9d46aeb38f50f
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-07-23 15:13:46 +0530

    Update Missing And Repeating Numbers.cpp

--- Missing And Repeating Numbers.cpp
+++ Missing And Repeating Numbers.cpp
@@ -41,3 +41,49 @@ vector<int> findMissingRepeatingNumbers(vector < int > a) {
 }
 // TC=O(N)
 // SC=O(1)
+
+
+// Approach using Bits Manipulation and XOR (slightly harder to understand for the first time)
+vector<int> findMissingRepeatingNumbers(vector < int > a) {
+    int n=a.size();
+    int xr=0;
+
+    for(int i=0; i<n; i++){
+        xr=xr^a[i];
+        xr=xr^(i+1);
+    }
+
+    int sigBit=0;
+    
+    while(1){
+        if((xr & (1<<sigBit))!=0) break;
+        sigBit++;
+    }
+
+    int oneClub=0;
+    int zeroClub=0;
+    for(int i=0; i<n; i++){
+        if((a[i]&(1<<sigBit))!=0){
+            oneClub=oneClub^a[i];
+        }else{
+            zeroClub=zeroClub^a[i];
+        }
+
+        if(((i+1)&(1<<sigBit))!=0){
+            oneClub=oneClub^(i+1);
+        }else{
+            zeroClub=zeroClub^(i+1);
+        }
+    }
+
+    int cnt=0;
+    for(int i=0; i<n; i++){
+        if(a[i]==zeroClub) cnt++;
+    }
+
+    if(cnt==2) return {zeroClub, oneClub};
+    return {oneClub, zeroClub};
+    
+}
+// TC=O(N)
+// SC=O(1)
