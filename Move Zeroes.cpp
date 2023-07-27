76687638bc1b84e25751a0dfc976e2bb195c1254
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-07-27 16:07:46 +0530

    Update Move Zeroes.cpp

--- Move Zeroes.cpp
+++ Move Zeroes.cpp
@@ -1,3 +1,21 @@
+// Easy to understand Solution
+vector<int> moveZeros(int n, vector<int> arr) {
+    int k=0;
+    for(int i=0; i<n; i++){
+        if(arr[i]!=0){
+            arr[k]=arr[i];
+            k++;
+        }
+    }
+
+    for(int i=k; i<n; i++){
+        arr[i]=0;
+    }
+
+    return arr;
+}
+
+// 'Hard to understand personally for me' Solution.
 void moveZeroes(vector<int>& nums) {
         int i=0;
         int j=0;
@@ -9,3 +27,5 @@ void moveZeroes(vector<int>& nums) {
             }
         }
     }
+// TC=O(N)
+// SC=O(1)
