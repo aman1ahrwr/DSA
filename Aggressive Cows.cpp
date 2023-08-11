6852e19a6550950473b27a12e14347de73588147
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-08-11 16:32:46 +0530

    Update Aggressive Cows.cpp

--- Aggressive Cows.cpp
+++ Aggressive Cows.cpp
@@ -1,66 +1,43 @@
-bool isPossible(int a[], int n, int cows, int minDist) {
-      int cntCows = 1;
-      int lastPlacedCow = a[0];
-      for (int i = 1; i < n; i++) {
-        if (a[i] - lastPlacedCow >= minDist) {
-          cntCows++;
-          lastPlacedCow = a[i];
+#include<bits/stdc++.h>
+
+int placeCows(vector<int> &stalls, int dist, int noCows){
+    int cntCows=1;
+    int lastInd=0;
+    for(int i=1; i<stalls.size(); i++){
+        if((stalls[i]-stalls[lastInd])>=dist && cntCows<noCows){
+            cntCows++;
+            lastInd=i;
         }
-      }
-      if (cntCows >= cows) return true;
-      return false;
-    }
 
-int largestMinCows(int a[], int n, int cows){
-      sort(a, a + n);
-
-      int low = 1, high = a[n - 1] - a[0];
-
-      while (low <= high) {
-        int mid = (low + high) >> 1;
-
-        if (isPossible(a, n, cows, mid)) {
-          low = mid + 1;
-        } else {
-          high = mid - 1;
-        }
-      }
-    return high;
-}
-
-//Better Slightly
-bool isPossible(vector<int> &stalls, int k, int n, int mid){
-    int cows = 1;
-    int initialPos = stalls[0];
-    for(int i=1; i<n; i++){
-        if(stalls[i]-initialPos>=mid){
-            initialPos=stalls[i];
-            cows++;
-            if(cows==k){
-                return 1;
-            }
+        if(cntCows==noCows){
+            return 1;
         }
     }
+
     return 0;
 }
 
 int aggressiveCows(vector<int> &stalls, int k)
 {
     sort(stalls.begin(), stalls.end());
-    int low=0;
-    int n=stalls.size();
-    int high = stalls[n-1];
+
+    int low=1;
+    int high=stalls[stalls.size()-1]-stalls[0];
     int ans=-1;
+
     while(low<=high){
         int mid=low+(high-low)/2;
-        
-        if(isPossible(stalls, k, n, mid)){
+
+        if(placeCows(stalls, mid, k)==1){
             ans=mid;
             low=mid+1;
         }else{
             high=mid-1;
         }
     }
-    
+
     return ans;
 }
+
+// TC=O(N*log(maxEle-minEle))
+// SC=O(1)
