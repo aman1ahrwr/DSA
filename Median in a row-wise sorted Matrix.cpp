c2246bca3a30c5ba4253a9c5d8e255478366586c
Aman Ahirwar <87198214+aman1ahrwr@users.noreply.github.com>
2023-08-18 17:37:06 +0530

    Update Median in a row-wise sorted Matrix.cpp

--- Median in a row-wise sorted Matrix.cpp
+++ Median in a row-wise sorted Matrix.cpp
@@ -1,3 +1,27 @@
+int median(vector<vector<int>> &matrix, int m, int n) {
+    int low=1;
+    int high=1e9;
+
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        int cnt=0;
+
+        for(int i=0; i<m; i++){
+            cnt+=upper_bound(matrix[i].begin(), matrix[i].end(), mid)-matrix[i].begin();
+        }
+
+        if(cnt<=((m*n)/2)){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+
+    return low;
+}
+// TC=O(32*N*logM)
+// SC=O(1)
+
 // Better, Easy to Understand Solution.
 int countSmaller(vector<int>& temp, int t){
         int low=0;
